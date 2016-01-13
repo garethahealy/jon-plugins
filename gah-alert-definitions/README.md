bc580e4e0adea7a944922de98967e395977bb104
Gareth Healy <garethahealy@gmail.com>
2016-01-13 11:29:39 +0000

    Added cpu alert

--- gah-alert-definitions/README.md
+++ gah-alert-definitions/README.md
@@ -17,7 +17,7 @@ If you are not sure what the 'pluginName' or 'resourceTypeName' for the Injected
     left join rhq_alert_condition ac ON ad.id = ac.alert_definition_id
     left join rhq_measurement_def md ON ac.measurement_definition_id = md.id
     where ad.parent_id = 0 and ad.name = '{alert name}'
-    order by ad.name asc;
+    order by ac.name asc;
 
 ## Important tables
 - rhq_alert_definition
--- gah-alert-definitions/src/main/java/com/garethahealy/jon/plugins/server/gah/alert/defintions/templates/cpu/CPUCoreNotRunningTemplate.java
+++ gah-alert-definitions/src/main/java/com/garethahealy/jon/plugins/server/gah/alert/defintions/templates/cpu/CPUCoreNotRunningTemplate.java
@@ -19,19 +19,79 @@
  */
 package com.garethahealy.jon.plugins.server.gah.alert.defintions.templates.cpu;
 
+import java.util.Map;
+
 import com.garethahealy.jon.plugins.server.gah.alert.defintions.InjectedTemplate;
 
+import org.rhq.core.domain.alert.AlertCondition;
+import org.rhq.core.domain.alert.AlertConditionCategory;
+import org.rhq.core.domain.alert.AlertDefinition;
+import org.rhq.core.domain.alert.AlertPriority;
+import org.rhq.core.domain.alert.BooleanExpression;
+import org.rhq.core.domain.measurement.MeasurementDefinition;
 import org.rhq.core.domain.resource.ResourceType;
 
 public class CPUCoreNotRunningTemplate extends InjectedTemplate {
 
+    private static final String DOWN_NAME = "AVAIL_GOES_DOWN";
+    private static final String NOT_UP_NAME = "AVAIL_GOES_NOT_UP";
+    private static final String DISABLED_NAME = "AVAIL_GOES_DISABLED";
+    private static final String UNKNOWN_NAME = "AVAIL_GOES_UNKNOWN";
+
     public CPUCoreNotRunningTemplate() {
-        super("Platforms", "Linux", "LinuxVmNotRunning", "The linux VM is not running");
+        super("Platforms", "CPU", "CPUCoreNotRunning", "A CPU core is down, not up, disabled or unknown");
     }
 
     @Override
     public int inject(ResourceType resourceType) {
-        //TODO
-        return 0;
+        Map<String, MeasurementDefinition> metricDefinitions = getMetricDefinitionsMap(resourceType);
+
+        AlertDefinition alertDefinition = new AlertDefinition();
+        alertDefinition.setName(getName());
+        alertDefinition.setResourceType(resourceType);
+        alertDefinition.setPriority(AlertPriority.MEDIUM);
+        alertDefinition.setConditionExpression(BooleanExpression.ANY);
+        alertDefinition.setDescription(getDescription());
+        alertDefinition.setRecoveryId(0);
+        alertDefinition.setEnabled(true);
+        alertDefinition.addCondition(getDoesDownAlertCondition(metricDefinitions));
+        alertDefinition.addCondition(getNotUpAlertCondition(metricDefinitions));
+        alertDefinition.addCondition(getDisabledAlertCondition(metricDefinitions));
+        alertDefinition.addCondition(getUnknownAlertCondition(metricDefinitions));
+
+        int newTemplateId = create(resourceType, alertDefinition);
+        return newTemplateId;
+    }
+
+    private AlertCondition getDoesDownAlertCondition(Map<String, MeasurementDefinition> metricDefinitions) {
+        AlertCondition alertCondition = new AlertCondition();
+        alertCondition.setName(DOWN_NAME);
+        alertCondition.setCategory(AlertConditionCategory.AVAILABILITY);
+
+        return alertCondition;
+    }
+
+    private AlertCondition getNotUpAlertCondition(Map<String, MeasurementDefinition> metricDefinitions) {
+        AlertCondition alertCondition = new AlertCondition();
+        alertCondition.setName(NOT_UP_NAME);
+        alertCondition.setCategory(AlertConditionCategory.AVAILABILITY);
+
+        return alertCondition;
+    }
+
+    private AlertCondition getDisabledAlertCondition(Map<String, MeasurementDefinition> metricDefinitions) {
+        AlertCondition alertCondition = new AlertCondition();
+        alertCondition.setName(DISABLED_NAME);
+        alertCondition.setCategory(AlertConditionCategory.AVAILABILITY);
+
+        return alertCondition;
+    }
+
+    private AlertCondition getUnknownAlertCondition(Map<String, MeasurementDefinition> metricDefinitions) {
+        AlertCondition alertCondition = new AlertCondition();
+        alertCondition.setName(UNKNOWN_NAME);
+        alertCondition.setCategory(AlertConditionCategory.AVAILABILITY);
+
+        return alertCondition;
     }
 }
